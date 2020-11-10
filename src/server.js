73827b481052a895f0bbb5fb9a1dc32e1062123e
Devon Mack <devon.mack@shopify.com>
2020-11-10 16:39:21 -0500

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import Discord from 'discord.js';
+import { config } from 'dotenv';
+
+// Load env variables
+config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client?.user?.tag}!`);
+});
+
+client.on('message', (msg) => {
+});
+
+client.login(process.env.TOKEN);
